d16cfb03275e0664b9a1a20355c81d28597b96b5
Hannah Tuttle <hannah@Hannahs-MacBook-Air.local>
2019-07-17 12:54:38 -0700

    adding header component

---
+++ components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from './nav.js'
+
+function Header() {
+    return <header>
+        <h1>NASA photo of the day!</h1>
+        <Nav />
+        </header>
+}
+
+export default Header
--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import Header from '../components/header.js'
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <section>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+     </section>
+      <footer>
+
+      </footer>
     </div>
   );
 }
